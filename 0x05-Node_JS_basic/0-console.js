6c9898321c2b29a3b97f22ba3d9962fb3f0e8189
DevEbrah <onyangohibrahim0@gmail.com>
2024-04-29 02:51:12 -0700

    Node Js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Hibrahim Onyango <https://github.com/DevEbrah>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
