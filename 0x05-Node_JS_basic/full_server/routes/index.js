7764eaa7154fa100ffbef8df2cdf7a5ddba652e2
DevEbrah <onyangohibrahim0@gmail.com>
2024-04-29 03:03:44 -0700

    Node Js

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,17 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+/**
+ * Binds the routes to the appropriate handler in the
+ * given Express application.
+ * @param {Express} app The Express application.
+ * @author Onyango Hibrahim <https://github.com/DevEbrah>
+ */
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
